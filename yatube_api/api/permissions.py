91ea2915f7d504f79266317e721bde4c752d3781
VtlBz <VtlBz@users.noreply.github.com>
2022-11-29 10:05:35 +0300

    HW9v2.2

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -4,14 +4,6 @@ from rest_framework import permissions
 class IsAuthorOrReadOnly(permissions.BasePermission):
     message = 'Запрещено изменение и/или удаление чужого контента!'
 
-    # def has_object_permission(self, request, view, obj):
-    #     if request.method in permissions.SAFE_METHODS:
-    #         return True
-    #     return obj.author == request.user
-
-    # Можно, но нужно ли?
-    # Тем более в одну строку всё равно не лезет...
-
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
                 or obj.author == request.user)
--- yatube_api/yatube_api/wsgi.py
+++ yatube_api/yatube_api/wsgi.py
@@ -1,19 +1,3 @@
-# Я дико извиняюсь, но нет.
-# Яндекс практически каждый проект даёт мне новую репу с дефолтными файлами,
-# и каждый раз шариться по стандартным файлам Джанго
-# выискивая и удалая дефолтные комменты и докстринги для прохождения ревью
-# это не совсем то, чем мне хочется (и вообще нужно) сейчас заниматься
-# Замечание принял к сведению, по поводу поддержания чистоты к коде в курсе.
-
-"""
-WSGI config for yatube_api project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
